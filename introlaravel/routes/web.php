5da056f19fb3bc86e0bb1af5e2faf263e857bbb6
UzielHernandesz <146143112+UzielHernandesz@users.noreply.github.com>
2024-09-26 18:57:00 -0600

    FIN PRACTICA 3

--- introlaravel/routes/web.php
+++ introlaravel/routes/web.php
@@ -3,5 +3,23 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('inicio');
+
+})->name('rutaInicio');
+
+
+Route::get('/', function () {
+    return view('formulario');
+
+})->name('rutaForm');
+
+
+Route::get('/consultar', function () {
+    return view('clientes');
+
+})->name('rutaClientes');
+
+
+Route::view('/','Inicio')-> name('rutaInicio');
+Route::view('/form','formulario')->name('rutaForm');
+Route::view('/consultar','clientes')-> name('rutaClientes');
