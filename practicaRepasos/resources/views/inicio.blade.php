056a4ac8d15590deef6f6ecb9be5267c37fe92c8
UzielHernandesz <146143112+UzielHernandesz@users.noreply.github.com>
2024-10-17 14:42:43 -0600

    CREACION VISTA INICIO PASO 3

---
+++ practicaRepasos/resources/views/inicio.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <H1> Vista Inicio Datos Personales </H1>
+    <h2>Nombre: Vargas Hernandez Uziel Oswaldo </h2>
+    <h2>Grupo: S-193 </h2>
+    <h2>Materia: Programcion web </h2>
+
+</body>
+</html>
